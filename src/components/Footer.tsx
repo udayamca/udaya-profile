7a4667a7d37586811ace95e308ef2ca3c4ffaa66
udaya <udayamca@gmail.com>
2026-05-12 20:40:55 -0700

    fix: removed footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,7 @@ export default function Footer() {
   return (
     <>
       <div className="h-10 bg-slate-900 text-white text-center font-light text-xs p-4 fixed bottom-0 w-full border-t-1 border-gray-800">
-        © {year} Udaya Chandar Kannan Parameswaran · Staff Frontend Engineer ·
-        Fremont, CA
+        © {year} Udaya Chandar · Fremont, CA
       </div>
     </>
   );
